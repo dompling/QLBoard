d245cc7ee961782a3e7a317a776304906642895e
dompling <374779789@qq.com>
2021-05-08 16:01:34 +0800

    initial project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
-const fabric = require('@umijs/fabric');
-
-module.exports = fabric.eslint;
+module.exports = {
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+  globals: {
+    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
+    page: true,
+    REACT_APP_ENV: true,
+  },
+  rules: {
+    'react/no-array-index-key': 'off',
+    'react-hooks/rules-of-hooks': 'off',
+    'no-console': 'off',
+  },
+};
